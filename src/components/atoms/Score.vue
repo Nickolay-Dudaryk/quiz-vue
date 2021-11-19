ba0c5d2bfbf7d1f6d96c69c3731d67f5538031a9
Nickolay Dudaryk <nick.dudaryk@ester.co>
2021-11-19 12:18:07 +0300

    feat: current question number counter

--- src/components/atoms/Score.vue
+++ src/components/atoms/Score.vue
@@ -1,8 +1,18 @@
 <template>
-  <p>Score: {{ $store.state.correctAnswers }} of {{ $store.state.questionsAmount }}</p>
+  <div class="score-block">
+    <p>Score: {{ $store.state.correctAnswers }} of {{ $store.state.questionsAmount }}</p>
+    <p>Question {{ $store.state.currentQuestionIdx + 1 }} of {{ $store.state.questionsAmount }}</p>
+  </div>
 </template>
 
 <script>
 export default {
 };
 </script>
+<style lang="scss">
+  .score-block {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
